389ce7e3eb8bde86ed4471cc08ebb5479fd0ad07
nick <nickagliano@gmail.com>
2020-04-22 09:14:37 -0400

    update gitignore

---
+++ build/precache-manifest.6a844fe6679615690b2d6464aadfccf3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7449ab5812577483237ef833194e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b625585d1f2ce74cb58",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "72a1522372b60500b725",
+    "url": "/static/css/main.bee03b0e.chunk.css"
+  },
+  {
+    "revision": "0b625585d1f2ce74cb58",
+    "url": "/static/js/2.52dc75dd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.52dc75dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a1522372b60500b725",
+    "url": "/static/js/main.4b5efcb5.chunk.js"
+  },
+  {
+    "revision": "20b7bf9944ca97e93660",
+    "url": "/static/js/runtime-main.3bb718a7.js"
+  },
+  {
+    "revision": "6279d13dd0660ca4197533f08923754a",
+    "url": "/static/media/logo.6279d13d.png"
+  }
+]);
